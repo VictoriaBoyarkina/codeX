5bcd102dbae4bdd767401970ac8ad99dc26f95e4
Виктория Бояркина <viktoriaboarkina@MacBook-Air-uzivatela-Viktoria.local>
2025-05-05 21:13:52 +0300

    feature/task-0 добавила переменные окружения и дополнила ридми

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+      },
     },
-  },
-  server: {
-    proxy: { "/codex": "http://localhost:3000" },
-  },
+    server: {
+      proxy: { "/codex": env.VITE_API_URL || "http://localhost:3000" },
+    },
+  };
 });
